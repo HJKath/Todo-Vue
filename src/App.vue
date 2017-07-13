29ba48b9dee0471c0ab6dd288553f8b7a8d4a043
hjkath <happyhj8711@gmail.com>
2017-07-13 21:10:32 +0900

    clear local storage method 추가

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <div id="app">
     <InputComponent v-bind:passedData="todoItems"></InputComponent>
     <ListComponent v-bind:passedData="todoItems" v-on:removeItem="removeItem"></ListComponent>
+    <FooterComponent v-bind:passedData="todoItems" v-on:clearLocalStorage="clearLocalStorage" v-if="todoItems != null && todoItems.length > 0"></FooterComponent>
   </div>
 </template>
 
 <script>
 import InputComponent from './components/InputComponent.vue'
 import ListComponent from './components/ListComponent.vue'
+import FooterComponent from './components/FooterComponent.vue'
 
 export default {
   name: 'app',
@@ -21,6 +23,7 @@ export default {
   ,components : {
     ListComponent: ListComponent
     ,InputComponent: InputComponent
+    ,FooterComponent: FooterComponent
   }
   ,created() {
     for(var key in localStorage) {
@@ -42,6 +45,11 @@ export default {
       var key = this.todoItems[index];
       this.todoItems.splice(index, 1);
       localStorage.removeItem(key);
+    }
+    ,clearLocalStorage() {
+      console.log(">> clear local storage");
+      localStorage.clear();
+      this.todoItems = [];
     }
   }
 }
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="FooterComp">
+
+    <button class="btn-clear" v-on:click="clearLocalStorage">Clear All</button>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // props: ['passedData'],
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    clearLocalStorage () {
+      this.$emit('clearLocalStorage');
+    }
+
+  },
+}
+</script>
+
+<style lang="css">
+  .btn-clear {
+    width: 100%;
+    height: 34px;
+  }
+</style>
